19af7c8c6b3f91e6647b81db7aa2b31483de76c0
James Leal Vieira <james.leal2@gmail.com>
2021-08-16 23:00:35 -0300

    Integrating faunadb to control the users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna as Fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,23 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await Fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
+            q.Create(q.Collection('users'), { data: { email: email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
 });
