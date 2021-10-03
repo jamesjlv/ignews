3b82d2a9ff8e8d31be13ab66f7354439c65a3376
James Leal Vieira <james.leal2@gmail.com>
2021-10-03 20:48:28 -0300

    Adding tests with jest

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="Logotipo ignews" />
+        <Image src="/images/logo.svg" alt="Logotipo ignews" width={100} height={100} />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
-import { query as q } from 'faunadb';
-
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-
-import { fauna as Fauna } from '../../../services/fauna';
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
   providers: [
@@ -14,10 +12,35 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
+                )
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       try {
-        await Fauna.query(
+        await fauna.query(
           q.If(
             q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
             q.Create(q.Collection('users'), { data: { email: email } }),
